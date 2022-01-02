ff184c696bab7a34664b29dd67f482e5d9dc1f85
Minhyeok-Oh <omh34658981@gmail.com>
2022-01-02 13:50:16 +0900

    init

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: `zoomkoding.com`,
-  description: `줌코딩의 개발일기`,
+  title: `minhye0k`,
+  description: `개발 숲`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://www.zoomkoding.com`,
+  siteUrl: `https://minhye0k.github.io/`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
@@ -11,10 +11,10 @@ module.exports = {
   },
   ga: '0', // Google Analytics Tracking ID
   author: {
-    name: `정진혁`,
+    name: `오민혁`,
     bio: {
       role: `개발자`,
-      description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
+      description: ['커뮤니케이션에 가치를 두는'],
       thumbnail: 'sample.png', // Path to the image in the 'asset' folder
     },
     social: {
